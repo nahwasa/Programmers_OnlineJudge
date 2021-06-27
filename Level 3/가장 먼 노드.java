518ecddf43b62fc9502dd1aef3545f8e8fcdfdf2
Se-young Hwang <nahwasa@gmail.com>
2021-06-27 12:56:16 +0900

    [Level 3] 가장 먼 노드
    1부터 특정 노드 A까지의 '최단거리' 중 최대치를 찾아야 하므로,
    bfs로 일단 접근.
    다만 bfs 중 중간 종료조건은 없고, 그냥 전체를 다 돌려보면 됨.
    
    1. 11~13 line에서 edge를 좀 더 시간복잡도 좋은 녀석으로 변경
    2. 이후 그냥 1부터 시작해서 전체 bfs를 돌리면 됨.
    3. 최단경로 중 최대치인 노드의 갯수는 뭐 bfs 다 돌리고 따로 거리보고 정해도 되지만, 줄수가 늘어나므로 그냥 23~24 line에서 바로 구해줬음.
    4. dist 배열이 따로 없는 이유는 그냥 v 랑 같이 썼으므로임.

---
+++ Level 3/가장 먼 노드.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int n, int[][] edge) {
+        int[] v = new int[n+1]; //0;not yet visited, <0;distance
+
+        // make adjacent list
+        List[] adj = new List[n+1];
+        for (int i = 1; i <= n; i++)    adj[i] = new ArrayList<Integer>();
+        for (int[] tmp : edge) { adj[tmp[0]].add(tmp[1]);   adj[tmp[1]].add(tmp[0]); }
+
+        // solve
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        v[1] = 1;
+        int maxCnt = 0;
+        int maxNum = 0;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            if (v[cur] > maxNum)    { maxCnt = 0; maxNum = v[cur]; }
+            if (v[cur] == maxNum)   maxCnt++;
+            List<Integer> curAdj = adj[cur];
+            for (int i = 0; i < curAdj.size(); i++) {
+                int next = curAdj.get(i);
+                if (v[next] != 0) continue;
+                v[next] = v[cur] + 1;
+                q.add(next);
+            }
+        }
+        return maxCnt;
+    }
+}
