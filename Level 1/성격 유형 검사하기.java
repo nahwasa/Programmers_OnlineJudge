883af151f612373ceb2ad3d7da85a5aa4c227cc4
Se-young Hwang <nahwasa@gmail.com>
2022-08-18 23:33:34 +0900

    Update 성격 유형 검사하기.java

--- Level 1/성격 유형 검사하기.java
+++ Level 1/성격 유형 검사하기.java
@@ -1,3 +1,6 @@
+/**
+ * 문제 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+ */
 class Solution {
     private static final String[] INDICATORS = {"RT", "CF", "JM", "AN"};
     public String solution(String[] survey, int[] choices) {
