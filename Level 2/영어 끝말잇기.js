d105468fe3b9182fd6281ecb1bb982977adef0e2
Se-young Hwang <nahwasa@gmail.com>
2022-09-23 07:27:14 +0900

    [Level 2] 영어 끝말잇기 (JS)
    해설은 nahwasa.com 블로그에!

---
+++ Level 2/영어 끝말잇기.js
@@ -0,0 +1,16 @@
+function solution(n, words) {
+    let answer = [0,0];
+    let last = words[0][words[0].length-1];
+    let hs = new Set();
+    hs.add(words[0]);
+    for (let i = 1; i < words.length; i++) {
+        if (words[i][0] != last || hs.has(words[i])) {
+            answer[0] = i%n+1;
+            answer[1] = parseInt(i/n)+1;
+            break;
+        }
+        last = words[i][words[i].length-1];
+        hs.add(words[i]);
+    }
+    return answer;
+}
